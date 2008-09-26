747bf2c3841a29f99a7eb4e1e5c51962b20e774b
Colin Campbell-McPherson <colin@logaan.net>
2008-09-26 22:09:00 +0800

    generated authenticated user sessions

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  
 end
