a6e1451ac32d7fb6a2d27e5724d8fa302e3de8f2
buntine <andrew.buntine@ansearch.com>
2008-09-29 15:07:12 +1000

    Added edit form

--- spec/views/tasks/edit.html.erb_spec.rb
+++ spec/views/tasks/edit.html.erb_spec.rb
@@ -2,11 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/tasks/edit" do
   before(:each) do
+    @task = mock_model(Task, :id => 1, :name => 'cheese')
+
+    assigns[:task] = @task
+
     render 'tasks/edit'
   end
   
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/tasks/edit])
+  it "should have a form for entering task details" do
+    response.should have_tag('form#edit_task_1')
   end
+
+  it "should have a labelled name field" do
+    response.should have_tag("label[for=task_name]")
+    response.should have_tag("#task_name")
+  end
+
+  it "should preload current value into task_name text field" do
+    response.should have_tag("#task_name[value='cheese']")
+  end
+
 end
