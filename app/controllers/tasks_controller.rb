88f47d7dd95e8084ab41319ead6397472bb2f98c
buntine <andrew.buntine@ansearch.com>
2008-09-29 12:31:18 +1000

    Added create action to tasks, shuffled spec stuff

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_filter :login_required, :only => [ :new, :create, :edit, :update, :destroy ]
+
   def index
     @tasks = if params[:user_id]
       User.find(params[:user_id]).tasks
@@ -12,6 +14,16 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new params['task']
+    @task.user = current_user
+
+    if @task.valid?
+      @task.save
+      flash[:notice] = "New task created successfully!"
+      redirect_to tasks_path
+    else
+      render :action => 'new'
+    end
   end
 
   def show
